a77735e363c725e6429d3322c68bdd87aa2eea6a
yue <yuetianyu@daniel-soft.com.cn>
2016-07-20 11:47:23 +0800

    test

---
+++ frmFilterOption.vb
@@ -0,0 +1,10 @@
+﻿Public Class frmFilterOption
+
+    Private Sub btnOk_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOk.Click
+
+
+        Me.DialogResult = Windows.Forms.DialogResult.OK
+        Me.Close()
+    End Sub
+
+End Class
